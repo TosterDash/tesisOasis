dee9fafe459d74b3cd63c6483ea49d0cc4848926
TosterDash <43210121+TosterDash@users.noreply.github.com>
2021-07-06 21:02:54 -0600

    Creacion de base de datos
    Pues ahi nomas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('nombreUsuario');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
