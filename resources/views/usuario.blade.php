8768858068ef9170488b44132ac2372d11994b25
MegaSupertacoman <86704079+MegaSupertacoman@users.noreply.github.com>
2021-07-15 22:53:00 -0500

    Create usuario.blade.php

---
+++ resources/views/usuario.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/head')
+
+@section('bodyPage')
+
+<p><label for="name">usuario:</label> <input id="name" maxlength="8" name="name" required="" size="10" type="text" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&iquest;no esta registrado? crea su cuenta nueva ahora mismo</p>
+<p><label for="password">contrase&ntilde;a:</label> <input id="password" maxlength="8" name="password" required="" size="10" type="text" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; (Boton de registro)</p>
+<p>(Boton de inicio)</p>
