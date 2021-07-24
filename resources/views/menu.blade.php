bb82980399de47c471faa41afb9c2847aa1d1ddb
MegaSupertacoman <86704079+MegaSupertacoman@users.noreply.github.com>
2021-07-23 23:24:20 -0500

    menus

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,58 +1,108 @@
 @extends('layouts/head')
 
+@section('titlePage',"Tacos Oasis")
+
 @section('bodyPage')
-//  /* <--!aqui hira el codigo HTML--> */
+<!--aqui hira el codigo HTML-->
 <html>
-<table class="editorDemoTable" style="height: 362px;">
-<thead>
-<tr style="height: 18px;">
-<td style="height: 18px; width: 262px;">menu</td>
-<td style="height: 18px; width: 349px;">menu</td>
-<td style="height: 18px; width: 56px;">botones</td>
-</tr>
-</thead>
-<tbody>
-<tr style="height: 22px;">
-<td style="height: 22px; width: 262px;">Taco Arabe</td>
-<td style="height: 22px; width: 349px;">$21</td>
-<td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-<tr style="height: 22px;">
-<td style="height: 22px; width: 262px;">Taco Arabe con queso</td>
-<td style="height: 22px; width: 349px;">$28</td>
-<td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-<tr style="height: 22px;">
-<td style="height: 22px; width: 262px;">Taco Oriental</td>
-<td style="height: 22px; width: 349px;">$10</td>
-<td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-<tr style="height: 22px;">
-<td style="height: 22px; width: 262px;">Gringa</td>
-<td style="height: 22px; width: 349px;">$40</td>
-<td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-<tr style="height: 36px;">
-<td style="height: 36px; width: 262px;">Torta</td>
-<td style="height: 36px; width: 349px;">$21</td>
-<td style="height: 36px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-<tr style="height: 18px;">
-<td style="height: 18px; width: 262px;">Torta con Queso</td>
-<td style="height: 18px; width: 349px;">$28</td>
-<td style="height: 18px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-<tr style="height: 22px;">
-<td style="height: 22px; width: 262px;">Refresco</td>
-<td style="height: 22px; width: 349px;">$14</td>
-<td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-<tr style="height: 22px;">
-<td style="height: 22px; width: 262px;">Flan</td>
-<td style="height: 22px; width: 349px;">$9&nbsp;</td>
-<td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
-</tr>
-</tbody>
-</table>
+    <body class="background-body">
+        <div class="container my-5 px-0">
+            <div class="container background-wood1 ">
+                <div class="row">
+                  <div class="col-4">
+                      <form>
+                        <table class="editorDemoTable" style="height: 362px;">
+                            <thead>
+                            <tr style="height: 18px;">
+                            <td style="height: 18px; width: 262px;">
+                            <h4>menu</h4>
+                            </td>
+                            <td style="height: 18px; width: 349px;">
+                            <h4>menu</h4>
+                            </td>
+                            <td style="height: 18px; width: 63px;">
+                            <h4>botones</h4>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 262px;">
+                            <h4>Taco Arabe</h4>
+                            </td>
+                            <td style="height: 22px; width: 349px;">
+                            <h4>$21</h4>
+                            </td>
+                            <td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="Barabe" name="Arabe" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 262px;">
+                            <h4>Taco Arabe con queso</h4>
+                            </td>
+                            <td style="height: 22px; width: 349px;">
+                            <h4>$28</h4>
+                            </td>
+                            <td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="BTqueso" name="Tqueso" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 262px;">
+                            <h4>Taco Oriental</h4>
+                            </td>
+                            <td style="height: 22px; width: 349px;">
+                            <h4>$10</h4>
+                            </td>
+                            <td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="BTOriental" name="TOriental" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 262px;">
+                            <h4>Gringa</h4>
+                            </td>
+                            <td style="height: 22px; width: 349px;">
+                            <h4>$40</h4>
+                            </td>
+                            <td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="BGringa" name="Gringa" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            <tr style="height: 36px;">
+                            <td style="height: 36px; width: 262px;">
+                            <h4>Torta</h4>
+                            </td>
+                            <td style="height: 36px; width: 349px;">
+                            <h4>$21</h4>
+                            </td>
+                            <td style="height: 36px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="BTorta" name="Torta" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            <tr style="height: 18px;">
+                            <td style="height: 18px; width: 262px;">
+                            <h4>Torta con Queso</h4>
+                            </td>
+                            <td style="height: 18px; width: 349px;">
+                            <h4>$28</h4>
+                            </td>
+                            <td style="height: 18px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="BToQueso" name="ToQueso" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 262px;">
+                            <h4>Refresco</h4>
+                            </td>
+                            <td style="height: 22px; width: 349px;">
+                            <h4>$14</h4>
+                            </td>
+                            <td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="BRefresco" name="Refresco" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 262px;">
+                            <h4>Flan</h4>
+                            </td>
+                            <td style="height: 22px; width: 349px;">
+                            <h4>$9&nbsp;</h4>
+                            </td>
+                            <td style="height: 22px; width: 56px;"><strong style="font-size: 17px; color: #2b2301;"><button id="BFlan" name="Flan" type="button">A&ntilde;adir</button></strong></td>
+                            </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                </div>
+            </div>
+        </div>    
+    </body>
 
 @endsection
---
+++ resources/views/menuworld.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts/head')
+
+@section('titlePage',"Tacos Oasis")
+
+@section('bodyPage')
+<!--aqui hira el codigo HTML-->
+<html>
+    <body class="background-body">
+        <div class="container my-5 px-0">
+            <div class="container background-wood1 ">
+                <div class="row">
+                  <div class="col-4">
+                      <form>
+                        <table class="editorDemoTable" style="height: 204px; width: 674px;">
+                            <thead>
+                            <tr style="height: 18px;">
+                            <td style="height: 18px; width: 253px;">
+                            <h4>menu</h4>
+                            </td>
+                            <td style="height: 18px; width: 336px;">
+                            <h4>menu</h4>
+                            </td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 253px;">
+                            <h4>Taco Arabe</h4>
+                            </td>
+                            <td style="height: 22px; width: 336px;">
+                            <h4>$21</h4>
+                            </td>
+                            
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 253px;">
+                            <h4>Taco Arabe con queso</h4>
+                            </td>
+                            <td style="height: 22px; width: 336px;">
+                            <h4>$28</h4>
+                            </td>
+                            
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 253px;">
+                            <h4>Taco Oriental</h4>
+                            </td>
+                            <td style="height: 22px; width: 336px;">
+                            <h4>$10</h4>
+                            </td>
+                            
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 253px;">
+                            <h4>Gringa</h4>
+                            </td>
+                            <td style="height: 22px; width: 336px;">
+                            <h4>$40</h4>
+                            </td>
+                            
+                            </tr>
+                            <tr style="height: 36px;">
+                            <td style="height: 36px; width: 253px;">
+                            <h4>Torta</h4>
+                            </td>
+                            <td style="height: 36px; width: 336px;">
+                            <h4>$21</h4>
+                            </td>
+                            
+                            </tr>
+                            <tr style="height: 18px;">
+                            <td style="height: 18px; width: 253px;">
+                            <h4>Torta con Queso</h4>
+                            </td>
+                            <td style="height: 18px; width: 336px;">
+                            <h4>$28</h4>
+                            </td>
+                            
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 253px;">
+                            <h4>Refresco</h4>
+                            </td>
+                            <td style="height: 22px; width: 336px;">
+                            <h4>$14</h4>
+                            </td>
+                            
+                            </tr>
+                            <tr style="height: 22px;">
+                            <td style="height: 22px; width: 253px;">
+                            <h4>Flan</h4>
+                            </td>
+                            <td style="height: 22px; width: 336px;">
+                            <h4>$9&nbsp;</h4>
+                            </td>
+                            
+                            </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                </div>
+            </div>
+        </div>    
+    </body>
+
+@endsection
